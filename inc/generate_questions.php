62a5b960c66304452ed39da1eefae20b16050b1f
Chizzy <chizoba.princess@gmail.com>
2019-08-22 15:22:18 -0400

    removed negative answer options and duplicate answers

--- inc/generate_questions.php
+++ inc/generate_questions.php
@@ -6,23 +6,20 @@ for ($i = 0; $i <= 9; $i++) {
     // Get random numbers to add
     $leftAdder = rand(1, 100);
     $rightAdder = rand(1, 100);
-    if ($leftAdder == $rightAdder) {
-        $rightAdder = rand(1, 100);
-    }
 
     // Calculate correct answer
     $correctAnswer = $leftAdder + $rightAdder;
 
     // Get incorrect answers within 10 numbers either way of correct answer
     // Make sure it is a unique answer
-    $firstIncorrectAnswer = $correctAnswer + rand(-10, 10);
-    if ($correctAnswer == $firstIncorrectAnswer) {
-        $firstIncorrectAnswer = $correctAnswer + rand(-10, 10);
+    $firstIncorrectAnswer = abs($correctAnswer + rand(-10, 10));
+    while ($correctAnswer == $firstIncorrectAnswer) {
+        $firstIncorrectAnswer = abs($correctAnswer + rand(-10, 10));
     }
 
-    $secondIncorrectAnswer = $correctAnswer + rand(-10, 10);
-    if ($firstIncorrectAnswer == $secondIncorrectAnswer || $correctAnswer == $secondIncorrectAnswer) {
-        $secondIncorrectAnswer = $correctAnswer + rand(-10, 10);
+    $secondIncorrectAnswer = abs($correctAnswer + rand(-10, 10));
+    while ($firstIncorrectAnswer == $secondIncorrectAnswer || $correctAnswer == $secondIncorrectAnswer) {
+        $secondIncorrectAnswer = abs($correctAnswer + rand(-10, 10));
     }
 
     // Add question and answer to questions array
